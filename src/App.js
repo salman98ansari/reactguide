0b2c6a7f00f1e6bd76f5129287313c0615dfe638
Salman Ansari <salman98101@gmail.com>
2021-01-09 02:21:18 +0530

    try

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Salman
+          Salman Ansari
         </p>
         <a
           className="App-link"
