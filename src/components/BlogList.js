7f2cbcfa7497b1512d1eeaec462e4e1c69a61962
Salman Ansari <salman98101@gmail.com>
2021-01-25 20:18:43 +0530

    react route

--- src/components/BlogList.js
+++ src/components/BlogList.js
@@ -1,3 +1,4 @@
+import { Link } from 'react-router-dom';
 const BlogList = (props) => {
     const blogs = props.salman;
     const title =props.title;
@@ -6,8 +7,10 @@ const BlogList = (props) => {
         <h3>{title}</h3>
         {blogs.map(blog => (
             <div className="blog-preview" key={blog.id} >
-            <h2>{ blog.title }</h2>
-            <p>Written by { blog.author }</p>
+                <Link to={`/blogs/${blog.id}`}>
+                <h2>{ blog.title }</h2>
+                <p>Written by { blog.author }</p>
+                </Link>
             </div>
         ))}
         </div>
