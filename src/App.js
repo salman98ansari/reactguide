4df28ee6c141d7982ea502adf4e0bd250d7840d0
Salman Ansari <salman98101@gmail.com>
2021-01-26 02:27:15 +0530

    complete

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Home from './components/Home';
 import {BrowserRouter as Router , Route, Switch} from 'react-router-dom';
 import Create from './components/Create';
 import BlogDetails from './components/BlogDetails';
+import NotFound from './components/NotFound';
 
 
 
@@ -22,6 +23,9 @@ function App() {
             </Route>
             <Route path="/blogs/:id">
               <BlogDetails/>
+            </Route>
+            <Route path="*">
+              <NotFound />
             </Route>
           </Switch>
           
